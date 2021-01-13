386e429298fc360e2690a5306eeed05839705b37
Roman Zheludkov <romzhel@mail.ru>
2021-01-13 16:52:31 +0300

    - TemplateEditor: listView => teatArea

--- src/main/java/ru/romzhel/app/controllers/GlossaryEditorController.java
+++ src/main/java/ru/romzhel/app/controllers/GlossaryEditorController.java
@@ -3,9 +3,8 @@ package ru.romzhel.app.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
 import ru.romzhel.app.entities.StringGlossary;
 import ru.romzhel.app.nodes.GlossaryNode;
 import ru.romzhel.app.nodes.GlossaryRootNode;
@@ -13,17 +12,14 @@ import ru.romzhel.app.nodes.Node;
 import ru.romzhel.app.services.GlossaryService;
 
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class GlossaryEditorController implements Initializable, NodeController<MainAppController> {
     @FXML
     TextField tfName;
     @FXML
-    ListView<String> lvItems;
-    @FXML
-    TextField tfNew;
-    @FXML
-    Button btnAdd;
+    TextArea taItems;
     @FXML
     Button btnSave;
     private MainAppController mainAppController;
@@ -31,17 +27,12 @@ public class GlossaryEditorController implements Initializable, NodeController<M
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        btnAdd.setOnAction(e -> {
-            if (tfNew.getText() != null && !tfNew.getText().isEmpty()) {
-                lvItems.getItems().add(tfNew.getText());
-                tfNew.setText("");
-            }
-        });
-
         btnSave.setOnAction(e -> {
             StringGlossary stringGlossary = new StringGlossary();
             stringGlossary.setName(tfName.getText());
-            lvItems.getItems().forEach(stringGlossary::add);
+            Arrays.stream(taItems.getText().split("\n"))
+                    .filter(s -> !String.valueOf(s).isEmpty())
+                    .forEach(s -> stringGlossary.add(String.valueOf(s)));
 
             if (instigatorNode instanceof GlossaryRootNode) {
                 mainAppController.glossaryRootNode.getChildren().add(new GlossaryNode(stringGlossary));
@@ -49,12 +40,6 @@ public class GlossaryEditorController implements Initializable, NodeController<M
             } else if (instigatorNode instanceof GlossaryNode) {
                 ((GlossaryNode) instigatorNode).setData(stringGlossary);
                 GlossaryService.getInstance().getGlossaryMap().put(stringGlossary.getName(), stringGlossary);
-            }
-        });
-
-        lvItems.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.DELETE) {
-                lvItems.getItems().remove(lvItems.getSelectionModel().getSelectedItem());
             }
         });
     }
@@ -68,7 +53,7 @@ public class GlossaryEditorController implements Initializable, NodeController<M
             StringGlossary stringGlossary = ((GlossaryNode) instigatorNode).getData();
 
             tfName.setText(stringGlossary.getName());
-            lvItems.getItems().addAll(stringGlossary.getGlossaryItems());
+            taItems.setText(String.join("\n", stringGlossary.getGlossaryItems()));
         }
     }
 }
